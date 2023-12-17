cf444aabf5306ea22a7a9cfb5971ec1d6bd72f68
nazimfeni <mdnazimuddin77@gmail.com>
2023-12-17 17:05:08 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps/home_screen.dart';
+
+void main() {
+  runApp(const GoogleMapsApp());
+}
+
+class GoogleMapsApp extends StatelessWidget {
+  const GoogleMapsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
